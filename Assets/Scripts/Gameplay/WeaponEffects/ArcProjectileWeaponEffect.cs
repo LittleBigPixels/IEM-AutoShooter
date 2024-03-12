888a1bffa6a58375a02da652c693410f171f74ca
Roucool_PvP <lucastag77@gmail.com>
2024-03-12 15:04:48 +0100

    TURBO ARMES

---
+++ Assets/Scripts/Gameplay/WeaponEffects/ArcProjectileWeaponEffect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcProjectileWeaponEffect : IWeaponEffect
+{
+    private readonly BulletComponent m_prefab;
+    private readonly int m_multiShotCount;
+    private readonly float m_speed;
+    private readonly float m_rateOfFire;
+
+    private float m_nextShotDelay = 0;
+
+    public ArcProjectileWeaponEffect(BulletComponent prefab, int multiShotCount, float speed, float rateOfFire)
+    {
+        m_prefab = prefab;
+        m_multiShotCount = multiShotCount;
+        m_speed = speed;
+        m_rateOfFire = rateOfFire;
+    }
+
+    public void UpdateAndShoot(Vector3 origin, Vector3 direction)
+    {
+        float delayBetweenShots = 1.0f / m_rateOfFire;
+        m_nextShotDelay += Time.deltaTime;
+        if (m_nextShotDelay > delayBetweenShots)
+        {
+            Shoot(origin, direction);
+            m_nextShotDelay -= delayBetweenShots;
+        }
+    }
+
+    private void Shoot(Vector3 origin, Vector3 direction)
+    {
+        float angleOffset = 180.0f / (m_multiShotCount + 1);
+        float angle = -90 + angleOffset;
+
+        for (int i = 0; i < m_multiShotCount; i++)
+        {
+            BulletComponent bullet = GameObject.Instantiate<BulletComponent>(m_prefab);
+            bullet.transform.position = origin;
+            bullet.Velocity = Quaternion.AngleAxis(angle, Vector3.up) * direction * m_speed;
+            angle += angleOffset;
+        }
+    }
+}
