06330fc5fabc7ff57b5e181f7e9078f472ec0743
Roucool_PvP <lucastag77@gmail.com>
2024-03-12 16:15:24 +0100

    [NW] Hitting player and ennemies

--- Assets/Scripts/Gameplay/WeaponEffects/FireballComponent.cs
+++ Assets/Scripts/Gameplay/WeaponEffects/FireballComponent.cs
@@ -2,34 +2,28 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireballComponent : MonoBehaviour
+public class FireballComponent : BulletComponent
 {
     [SerializeField]
-    private float Speed;
-    [SerializeField]
     private int PassThrough = 5;
-
-    public Vector3 Direction
-    {
-        get => Direction;
-        set => Direction = value.normalized;
-    }
-    
-    public void Update()
-    {
-        transform.position += Time.deltaTime * Speed * Direction;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         EnemyComponent enemyComponent = other.gameObject.GetComponent<EnemyComponent>();
-        if (enemyComponent != null)
+        if (enemyComponent != null && IsOwnerPlayer)
         {
             enemyComponent.ApplyDamage(1);
             PassThrough--;
 
             if (PassThrough <= 0)
                 Destroy(gameObject);
+        }
+
+        PlayerComponent playerComponent = other.gameObject.GetComponent<PlayerComponent>();
+        if (playerComponent != null && !IsOwnerPlayer)
+        {
+            playerComponent.ApplyDamage(1);
+            Destroy(gameObject);
         }
     }
 }
